5f921648d467776aefd79abf3b04c464ff23f530
NODASHUSEINANODA <n.shusei1120@gmail.com>
2022-10-21 17:56:12 +0900

    ♻️ エクスポートデフォルトの削除

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Footer } from '../components/Footer'
-import Headline from '../components/Headline'
-import Links from '../components/Links'
+import { Headline } from '../components/Headline'
+import { Links } from '../components/Links'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export function Home() {
   return (
     <div className={styles.container}>
       <Head>
